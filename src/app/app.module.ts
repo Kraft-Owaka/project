fde449beec3f4e9ceb713c4b09504941a4e4b501
kraft-owaka <owakakraft@gmail.com>
2020-02-18 12:41:48 +0300

    Create forms.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RegistrationFormComponent } from './registration-form/registration-form.component';
 import { LoginFormComponent } from './login-form/login-form.component';
+import { CarFormComponent } from './car-form/car-form.component';
+import { WorkspaceFormComponent } from './workspace-form/workspace-form.component';
+import { ReviewFormComponent } from './review-form/review-form.component';
+import { TripFormComponent } from './trip-form/trip-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     RegistrationFormComponent,
     LoginFormComponent,
+    CarFormComponent,
+    WorkspaceFormComponent,
+    ReviewFormComponent,
+    TripFormComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/workspace-form/workspace-form.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { WorkspaceFormComponent } from './workspace-form.component';
+
+describe('WorkspaceFormComponent', () => {
+  let component: WorkspaceFormComponent;
+  let fixture: ComponentFixture<WorkspaceFormComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ WorkspaceFormComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(WorkspaceFormComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
